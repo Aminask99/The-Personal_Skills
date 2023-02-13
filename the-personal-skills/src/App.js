7ee6505e61501ecbbe0a22712725b5b80e705678
amina <aminasalik012@gmail.com>
2023-02-13 19:52:42 +0100

    la stroctur de project

---
+++ the-personal-skills/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Dash from './componant/Dash'
+
+function App() {
+  return (
+    <div className="App">
+   <Dash/>
+    </div>
+  );
+}
+
+export default App;
---
+++ the-personal-skills/src/componant/Dash.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import './style.css'
+export default function dash() {
+  return (
+   
+
+    <div align="center" class="fond">
+    
+        <a href="www.wifeo.com">
+        <div class="principal">
+          <div class="principal_petit">
+            <div class="principal_img">
+              <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/173024/img_scale_sociaux.png" border="0" align="center"/>
+            </div>
+          </div>
+        </div>
+      </a>
+    
+      <div style=" padding:5px; color:#ffffff; font-weight:300; font-size:30px; font-family:'Roboto';padding-top:20px;">Only CSS <font style="font-weight:400;">HOVER</font></div>
+            <a href="http://www.wifeo.com/code" style="text-decoration:none;" target="parent"><div style="  color:#ffffff; font-weight:300; font-size:20px; font-family:'Roboto';">www.wifeo.com/code</div></a>
+      
+      
+    </div>
+  )
+}
